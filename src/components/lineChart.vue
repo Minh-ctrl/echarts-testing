c7c0b93a0f8037e39c45a852c90c4cc6509c0d98
minh-ctrl <binhminh140899@gmail.com>
2022-04-10 18:49:20 +0900

    feat: 🎸 still learning

---
+++ src/components/lineChart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page-line-chart">
+    <VChart class="chart" :chartData="chartData"></VChart>
+    <div>this running?</div>
+  </div>
+</template>
+ 
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { BarChart } from "echarts/charts";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, defineComponent } from "vue";
+use([
+  CanvasRenderer,
+  BarChart,
+]);
+export default defineComponent({
+    components:{
+        VChart
+    },
+      provide: {
+    [THEME_KEY]: "light"
+  },
+    setup(){
+       const  chartData = ref({
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']},
+        yAxis: {type: 'value'},
+        series: [
+            {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar',
+                showBackground: true,
+                backgroundStyle: {
+                    color: 'rgba(180, 180, 180, 0.2)'
+                    }
+                }
+            ]
+            })
+                return {chartData} 
+        }
+})
+</script>
+
+<style scoped>
+    .chart{
+        height: 600px;
+        width: 600px;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import pieChart from '../components/pieChart.vue';
+import lineChart from '../components/lineChart.vue'
+import Test from '../components/Test.vue'
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <!-- <pieChart/> -->
+    <Test/>
+  </div>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
